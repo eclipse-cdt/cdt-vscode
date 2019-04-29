2b6ff006ca3d67a8543681cdc319a5499522782b
Doug Schaefer <dschaefer@blackberry.com>
2019-04-29 16:32:00 -0400

    Add ability to download clangd from the web.
    Also created a README in prep for first milestone release.

---
+++ README.md
@@ -0,0 +1,15 @@
+# VS Code Extension for C/C++ Development
+
+> Note: This is a preliminary milestone release. Please help us by trying out this extension and providing feedback using [our github issues page](https://github.com/eclipse-cdt/cdt-vscode/issues).
+
+This is a Visual Studio Code extension that supports C/C++ development and is brought to you by the same experts who built the C/C++ IDE for Eclipse. It is built using the new IDE architecture popularized by the Visual Studio Code team and includes the clangd language server as well as the CDT gdb debug adapter.
+
+The main focus of this extension is to serve the same community that used the C/C++ IDE for Eclipse but who are looking for the more editor focus experience that Visual Studio Code. It provides a platform to integrate build systems, compilers, and debuggers that are used by embedded system, server, and desktop developers. We provide exemplary implementations for the GNU toolchain but provide interfaces for other toolchain vendors to extend such as IAR.
+
+## Settings
+
+TODO
+
+## Build
+
+[Instructions to build are on the wiki](https://github.com/eclipse-cdt/cdt-vscode/wiki/Building)
--- src/extension.ts
+++ src/extension.ts
@@ -8,9 +8,63 @@
  * SPDX-License-Identifier: EPL-2.0
  *********************************************************************/
 import * as vscode from 'vscode';
-import { ClangdLanguageClient } from './clangd';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as request from 'request';
+import * as util from 'util';
+import * as mkdirp from 'mkdirp';
+import * as tar from 'tar';
 
-export function activate(context: vscode.ExtensionContext) {
-    // init the language server
-    new ClangdLanguageClient(context);
+async function downloadFile(url: string, dest: string) {
+    return vscode.window.withProgress({
+        location: vscode.ProgressLocation.Window,
+        title: 'Downloading and installing clangd'
+    }, (progress) => {
+        return new Promise((resolve, reject) => {
+            const file = fs.createWriteStream(dest);
+            request(url).pipe(file);
+            file.on('finish', () => { file.close(); resolve() });
+        });
+    })
+}
+
+export async function activate(context: vscode.ExtensionContext) {
+    const useBinaries = vscode.workspace.getConfiguration().get('cdt.clangd.binaries.enable');
+    if (useBinaries) {
+        const clangdDir = path.join(context.extensionPath, 'clangd');
+        const clangdExe = path.join(clangdDir, "/bin/clangd" + (process.platform === 'win32' ? ".exe" : ""));
+        const installed = await util.promisify(fs.exists)(clangdExe);
+        if (!installed) {
+            const doit = await vscode.window.showInformationMessage('Would you like to download and install clangd', 'yes', 'no');
+            if (doit === 'yes') {
+                await util.promisify(mkdirp)(clangdDir);
+                const clangdURL = vscode.workspace.getConfiguration().get('cdt.clangd.binaries.url') as string;
+                const manifestURL = clangdURL + 'manifest.json';
+                const manifestFile = path.join(clangdDir, 'manifest.json');
+                await downloadFile(manifestURL, manifestFile);
+                const manifest = require(manifestFile);
+                const tarball = manifest[process.platform];
+                await vscode.window.withProgress({
+                    location: vscode.ProgressLocation.Window,
+                    title: 'Downloading and installing clangd',
+                }, async (progress) => {
+                    await downloadFile(clangdURL + tarball, path.join(clangdDir, tarball));
+                    await tar.x({
+                        file: path.join(clangdDir, tarball),
+                        cwd: clangdDir
+                    });
+                });
+                vscode.workspace.getConfiguration().update('clangd.path', clangdExe, vscode.ConfigurationTarget.Global);
+                await vscode.window.showInformationMessage('Please reload the window to start the new clangd', 'Reload');
+                vscode.commands.executeCommand('workbench.action.reloadWindow');
+            }
+        } else {
+            const clangdPath = vscode.workspace.getConfiguration().get('clangd.path');
+            if (clangdPath !== clangdExe) {
+                vscode.workspace.getConfiguration().update('clangd.path', clangdExe, vscode.ConfigurationTarget.Global);
+                await vscode.window.showInformationMessage('Please reload the window to start the new clangd', 'Reload');
+                vscode.commands.executeCommand('workbench.action.reloadWindow');
+            }
+        }
+    }
 }
